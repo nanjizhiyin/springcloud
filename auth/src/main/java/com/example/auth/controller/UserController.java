78c6e2b44bbf7516a4294f0f8a6c2a4ddbcf6fb0
nanjizhiyin <nanjizhiyin@163.com>
2018-11-08 18:31:53 +0800

    添加了用户认证

---
+++ auth/src/main/java/com/example/auth/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author 郜金丹
+ * @Date 2018/11/8 17:37
+ */
+
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password) {
+
+        return "token123123";
+    }
+}
